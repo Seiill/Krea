c2059e424d25ab6c05f6661db9c3548353edc43b
Jarlin Estiven Quiñones Cabezas <90432771+Seiill@users.noreply.github.com>
2024-07-22 15:12:05 -0500

    Build aplication

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   define: {
     'import.meta.env.VITE_PUBLIC_URL': JSON.stringify('/public'), 
+  },
+  build: {
+    outDir: 'dist', 
   },
 })
